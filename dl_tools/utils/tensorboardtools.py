e51d3c016b08aa1478283b1c19d9e1e712f20417
Anders Dahl Henriksen <ahe@trackman.com>
2019-03-11 10:03:48 +0100

    -Expanded version compatibility of tensorboard tools

--- dl_tools/utils/tensorboardtools.py
+++ dl_tools/utils/tensorboardtools.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from tensorboard import default
 from tensorboard import program
 import logging
@@ -9,8 +10,12 @@ def tensorboard_launch(experiments_folder):
     logging.getLogger('werkzeug').setLevel(logging.ERROR)
     logging.getLogger('tensorflow').setLevel(logging.ERROR)
     # Start tensorboard server
-    tb = program.TensorBoard(default.PLUGIN_LOADERS, default.get_assets_zip_provider())
-    tb.configure(argv=['--logdir', experiments_folder])
+    if tf.VERSION == '1.10.0':
+        tb = program.TensorBoard(default.PLUGIN_LOADERS, default.get_assets_zip_provider())
+        tb.configure(argv=['--logdir', experiments_folder])
+    else:  # 1.12
+        tb = program.TensorBoard(default.get_plugins(), default.get_assets_zip_provider())
+        tb.configure(argv=[None, '--logdir', experiments_folder])
     url = tb.launch()
     sys.stdout.write('TensorBoard at %s \n' % url)
     # From: https://stackoverflow.com/questions/42158694/how-to-run-tensorboard-from-python-scipt-in-virtualenv/
