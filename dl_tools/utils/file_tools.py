8d5099e1ec7301eea9d18e811cd1527b54f79faf
Anders Dahl Henriksen <anders.d.henriksen@gmail.com>
2020-01-24 12:08:02 +0100

    -Changed data partitioning to be stratified

--- dl_tools/utils/file_tools.py
+++ dl_tools/utils/file_tools.py
@@ -4,7 +4,11 @@ from random import shuffle
 
 
 def partition_dataset(dataset_path, train_split=0.7, validation_split=0.15, test_split=0.15, overwrite_possible=False):
-    assert train_split + validation_split + test_split == 1, "Train + validation + test must equal 1"
+    print(f'Partitioning files from {dataset_path} ... ', end='')
+    if train_split + validation_split + test_split != 1:
+        print('Also rescaling split fractions to sum to 1 ...', end='')
+        train_split, validation_split, test_split = (split/(train_split + validation_split + test_split) for split in
+                                                     [train_split, validation_split, test_split])
 
     dataset_path = Path(dataset_path)
     dataset_path_all = dataset_path / "All"
@@ -18,16 +22,19 @@ def partition_dataset(dataset_path, train_split=0.7, validation_split=0.15, test
     entries_to_move = list([dp for dp in dataset_path.glob('*') if dp.stem not in new_folders])
     if not list(entries_to_move):
         return
-    print(f'Partitioning files from {dataset_path} ... ', end='')
     [dp.rename(dataset_path_all / dp.stem) for dp in entries_to_move]
 
-    # Get all files then shuffle
-    all_files = list(dataset_path_all.rglob("*.*"))
-    shuffle(all_files)
+    # Get all files, then shuffle while keeping stratified
+    partition_paths = [[], [], []]
+    category_folders = list(dataset_path_all.iterdir())
+    for category_folder in category_folders:
+        category_files = list(category_folder.rglob("*.*"))
+        shuffle(category_files)
+        split_idx = [int(train_split * len(category_files)), int((train_split + validation_split) * len(category_files))]
+        partition_paths[0] += category_files[split_idx[1]:]
+        partition_paths[1] += category_files[:split_idx[0]]
+        partition_paths[2] += category_files[split_idx[0]:split_idx[1]]
 
-    # Copy to Train, Val, Test folders
-    split_idx = [int(train_split * len(all_files)), int((train_split + validation_split) * len(all_files))]
-    partition_paths = [all_files[split_idx[1]:], all_files[:split_idx[0]], all_files[split_idx[0]:split_idx[1]]]
     for paths, name in zip(partition_paths, new_folders[1:]):
         for file_path in paths:
             dst = Path(file_path.as_posix().replace('/All/', f'/{name}/'))
@@ -37,4 +44,4 @@ def partition_dataset(dataset_path, train_split=0.7, validation_split=0.15, test
 
 
 if __name__ == "__main__":
-    partition_dataset(r"D:\2805 RMED Pre-project Droplet Detection Deep Learning\_Cutout", train_split=0.5, validation_split=0, test_split=.5)
+    partition_dataset(r"D:\SplitTest", train_split=0.5, validation_split=0.5, test_split=.5)
